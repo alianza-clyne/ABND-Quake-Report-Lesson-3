ebf46d4a0120058b0697f1d3ef4b2538e65f2645
Alianza <octavia1011@gmail.com>
2019-03-24 09:11:55 -0400

    Added internet permissions to AndroidManifest.xml. Changed name of MainActivity.java to EarthquakeActivity.java.

--- app/src/main/java/com/example/abnd_quakereport_lesson3/EarthquakeActivity.java
+++ app/src/main/java/com/example/abnd_quakereport_lesson3/EarthquakeActivity.java
@@ -3,7 +3,7 @@ package com.example.abnd_quakereport_lesson3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
